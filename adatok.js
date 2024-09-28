227c13f315bb9e7f499ba4c4bc64328c28f2802c
hunihunor <hunihunor90@gmail.com>
2024-09-28 16:44:26 +0200

    base project structure

---
+++ adatok.js
@@ -0,0 +1,27 @@
+const kajak = [
+    {név:"Doktor Kacsa" ,
+        leírás: "Vidám orvos kacsa, tökéletes fürdőtárs gyerekeknek és felnőtteknek egyaránt. Szórakoztató módon hoz egészséget a fürdőszobába!",
+        ár: 4500,
+        img: "https://i.ebayimg.com/images/g/mngAAOSwJ~digTBs/s-l1200.jpg"
+    },
+    {név:"Favágó Kacsa" ,
+        leírás: "Fából vaskarika? Nem, inkább fából gumikacsa! Tökéletes fürdőtárs, aki minden fürdést kalanddá varázsol.",
+        ár: 1250,
+        img: "https://e85bd63a214bee008f8f.cdn6.editmysite.com/uploads/b/e85bd63a214bee008f8f6e5e8f3bb58ea2477afa07603bbee8bec65421a40a92/lumberjack-rubber-duck-tree-stump_1600533138.jpg?width=2400&optimize=medium"
+    },
+    {név:"Mérges Kacsa" ,
+        leírás: "Ez a kacsa dühös! Haragos arckifejezésével ez a gumikacsa garantáltan feldobja a fürdőzést, akár gyerekek, akár felnőttek számára.",
+        ár: 1600,
+        img: "https://img.freepik.com/premium-photo/yellow-rubber-duck-with-angry-face_786255-40838.jpg"
+    },
+    {név:"Nyaraló Kacsa" ,
+        leírás: "Ez a kacsa már készen áll a strandra! Naptejjel, napszemüveggel és úszógumival felszerelve tökéletes társa lesz a nyári pancsolásoknak.",
+        ár: 3000,
+        img: "https://www.badeendwinkel.nl/8601/rubber-duck-beach-dr.jpg"
+    },
+    {név:"Kalóz Kacsa" ,
+        leírás: "Kalózkacsa a fedélzeten! Kendővel, szemfedővel és kampókézzel felszerelve ez a vidám kalóz kacsa igazi kincse a fürdőkádnak.",
+        ár: 8000,
+        img: "https://lh6.googleusercontent.com/proxy/cx-oWzCpEc073xiGYpifLJLXu_mjMmXosroDK4UMZMICTOYigpxKtIM-8eG97Vpfz2OR2M8tlZ68_FLO-Zs5Tecj1oH_a15wyKhf-s5uMogeyuuPI9dDxyDQCoyMgnuKrv0eQeJzCjG2rW38pvURdfvsCnXwDrntCOStWl6kD2kX6ok"
+    },
+]
