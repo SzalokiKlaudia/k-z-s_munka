69434457c7f22c80e0c4af67adf76ef9620a479a
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2024-10-03 13:13:55 +0200

    change className

---
+++ admin.js
@@ -0,0 +1,3 @@
+import AdminController from "./adminController/AdminController.js"
+
+new AdminController()
---
+++ adminController/adminController.js
@@ -0,0 +1,12 @@
+import AdminModel from "../adminModel/AdminModel.js"
+import Table from "../adminView/Table.js"
+
+export default class AdminController{
+    constructor(){
+        this.adminModel = new AdminModel()
+
+
+    }
+
+
+}
